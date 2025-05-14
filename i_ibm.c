45fa4da85c207c1a62cc602f8751036ab8c7810d
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2025-05-14 19:14:09 +0200

    IBM PC 16-bit: PC speaker sound effects

--- i_ibm.c
+++ i_ibm.c
@@ -26,7 +26,6 @@
 #include <conio.h>
 #include <dos.h>
 #include <stdarg.h>
-#include <time.h>
 
 #include "doomdef.h"
 #include "doomtype.h"
@@ -34,6 +33,7 @@
 #include "a_pcfx.h"
 #include "d_main.h"
 #include "i_system.h"
+
 #include "globdata.h"
 
 
@@ -254,28 +254,7 @@ void I_StartTic(void)
 
 //**************************************************************************************
 //
-// Audio
-//
-
-void PCFX_Play(int16_t lumpnum)
-{
-	// TODO implement me
-}
-
-void PCFX_Init(void)
-{
-	// TODO implement me
-}
-
-void PCFX_Shutdown(void)
-{
-	// TODO implement me
-}
-
-
-//**************************************************************************************
-//
-// Returns time in 1/35th second tics.
+// Low level timer task scheduler.
 //
 
 /*---------------------------------------------------------------------
@@ -291,7 +270,11 @@ typedef struct
 } task_t;
 
 
-#define MAX_TASKS 1
+#define LOBYTE(w)	(((uint8_t *)&(w))[0])
+#define HIBYTE(w)	(((uint8_t *)&(w))[1])
+
+
+#define MAX_TASKS 2
 #define MAX_SERVICE_RATE 0x10000L
 
 static task_t tasks[MAX_TASKS];
@@ -315,9 +298,6 @@ static boolean isTS_Installed = false;
 
    Sets the rate of the 8253 timer.
 ---------------------------------------------------------------------*/
-
-#define LOBYTE(w)	(((uint8_t *)&(w))[0])
-#define HIBYTE(w)	(((uint8_t *)&(w))[1])
 
 static void TS_SetClockSpeed(int32_t speed)
 {
@@ -494,6 +474,123 @@ static void TS_Terminate(int16_t priority)
 	_enable();
 }
 
+
+//**************************************************************************************
+//
+// Audio
+//
+
+#define PCFX_PRIORITY 1
+
+#define SND_TICRATE     140     // tic rate for updating sound
+
+
+static uint16_t	data[146];
+static int16_t	PCFX_LengthLeft;
+static const uint16_t *PCFX_Sound = NULL;
+static uint16_t	PCFX_LastSample = 0;
+
+static boolean	PCFX_Installed = false;
+
+
+static void PCFX_Stop(void)
+{
+	if (PCFX_Sound == NULL)
+		return;
+
+	_disable();
+
+	// Turn off speaker
+	outp(0x61, inp(0x61) & 0xfc);
+
+	PCFX_Sound      = NULL;
+	PCFX_LastSample = 0;
+
+	_enable();
+}
+
+
+static void PCFX_Service(void)
+{
+	if (PCFX_Sound)
+	{
+		uint16_t value = *PCFX_Sound++;
+
+		if (value != PCFX_LastSample)
+		{
+			PCFX_LastSample = value;
+			if (value)
+			{
+				outp(0x43, 0xb6);
+				outp(0x42, LOBYTE(value));
+				outp(0x42, HIBYTE(value));
+				outp(0x61, inp(0x61) | 0x3);
+			} else
+				outp(0x61, inp(0x61) & 0xfc);
+		}
+
+		if (--PCFX_LengthLeft == 0)
+		{
+			// Turn off speaker
+			outp(0x61, inp(0x61) & 0xfc);
+
+			PCFX_Sound      = NULL;
+			PCFX_LastSample = 0;
+		}
+	}
+}
+
+
+typedef struct {
+	uint16_t	length;
+	uint16_t	data[];
+} pcspkmuse_t;
+
+
+void PCFX_Play(int16_t lumpnum)
+{
+	PCFX_Stop();
+
+	const pcspkmuse_t __far* pcspkmuse = W_GetLumpByNum(lumpnum);
+	PCFX_LengthLeft = pcspkmuse->length;
+	_fmemcpy(data, pcspkmuse->data, pcspkmuse->length * sizeof(uint16_t));
+	Z_ChangeTagToCache(pcspkmuse);
+
+	_disable();
+
+	PCFX_Sound = &data[0];
+
+	_enable();
+}
+
+
+void PCFX_Init(void)
+{
+	if (PCFX_Installed)
+		return;
+
+	PCFX_Stop();
+	TS_ScheduleTask(&PCFX_Service, SND_TICRATE, PCFX_PRIORITY);
+
+	PCFX_Installed = true;
+}
+
+
+void PCFX_Shutdown(void)
+{
+	if (PCFX_Installed)
+	{
+		PCFX_Stop();
+		TS_Terminate(PCFX_PRIORITY);
+		PCFX_Installed = false;
+	}
+}
+
+
+//**************************************************************************************
+//
+// Returns time in 1/35th second tics.
+//
 
 #define TIMER_PRIORITY 0
 
