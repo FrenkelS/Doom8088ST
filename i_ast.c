cfe92b9f345fb9a10148de04acfc105bd8a383a8
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2025-06-24 22:44:16 +0200

    8 MB is the maximum amount of allocate memory

--- i_ast.c
+++ i_ast.c
@@ -414,7 +414,7 @@ unsigned int _dos_allocmem(unsigned int __size, unsigned int *__seg)
 	if (__size == 0xffff)
 	{
 		uint32_t availableMemory = Mxalloc(-1, MX_PREFTTRAM);
-		int32_t paragraphs = availableMemory < 1023 * 1024 ? availableMemory / PARAGRAPH_SIZE : 1023 * 1024L / PARAGRAPH_SIZE;
+		int32_t paragraphs = availableMemory < 8 * 1024 * 1024 ? availableMemory / PARAGRAPH_SIZE : 8 * 1024 * 1024L / PARAGRAPH_SIZE;
 		ptr = (uint8_t*)Mxalloc(paragraphs * PARAGRAPH_SIZE, MX_PREFTTRAM);
 		while (!ptr)
 		{
