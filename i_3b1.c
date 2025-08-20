66135d3d1751ca5c519881c477b5d7b0d7e3a716
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2025-08-20 22:29:12 +0200

    remove sound support

--- i_3b1.c
+++ i_3b1.c
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------
  *
  *
- *  Copyright (C) 2023-2025 Frenkel Smeijers
+ *  Copyright (C) 2025 Frenkel Smeijers
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
@@ -19,7 +19,7 @@
  *  02111-1307, USA.
  *
  * DESCRIPTION:
- *      DOS implementation of i_system.h
+ *      AT&T UNIX PC / PC 7300 / 3B1 implementation of i_system.h
  *
  *-----------------------------------------------------------------------------*/
 
@@ -480,110 +480,21 @@ static void TS_Terminate(int16_t priority)
 // Audio
 //
 
-#define PCFX_PRIORITY 1
-
-#define SND_TICRATE     140     // tic rate for updating sound
-
-
-static uint16_t	data[146];
-static int16_t	PCFX_LengthLeft;
-static const uint16_t *PCFX_Sound = NULL;
-static uint16_t	PCFX_LastSample = 0;
-
-static boolean	PCFX_Installed = false;
-
-
-static void PCFX_Stop(void)
-{
-	if (PCFX_Sound == NULL)
-		return;
-
-	_disable();
-
-	// Turn off speaker
-	outp(0x61, inp(0x61) & 0xfc);
-
-	PCFX_Sound      = NULL;
-	PCFX_LastSample = 0;
-
-	_enable();
-}
-
-
-static void PCFX_Service(void)
-{
-	if (PCFX_Sound)
-	{
-		uint16_t value = *PCFX_Sound++;
-
-		if (value != PCFX_LastSample)
-		{
-			PCFX_LastSample = value;
-			if (value)
-			{
-				outp(0x43, 0xb6);
-				outp(0x42, LOBYTE(value));
-				outp(0x42, HIBYTE(value));
-				outp(0x61, inp(0x61) | 0x3);
-			} else
-				outp(0x61, inp(0x61) & 0xfc);
-		}
-
-		if (--PCFX_LengthLeft == 0)
-		{
-			// Turn off speaker
-			outp(0x61, inp(0x61) & 0xfc);
-
-			PCFX_Sound      = NULL;
-			PCFX_LastSample = 0;
-		}
-	}
-}
-
-
-typedef struct {
-	uint16_t	length;
-	uint16_t	data[];
-} pcspkmuse_t;
-
-
 void PCFX_Play(int16_t lumpnum)
 {
-	PCFX_Stop();
-
-	const pcspkmuse_t __far* pcspkmuse = W_GetLumpByNum(lumpnum);
-	PCFX_LengthLeft = pcspkmuse->length;
-	_fmemcpy(data, pcspkmuse->data, pcspkmuse->length * sizeof(uint16_t));
-	Z_ChangeTagToCache(pcspkmuse);
-
-	_disable();
-
-	PCFX_Sound = &data[0];
-
-	_enable();
+	UNUSED(lumpnum);
 }
 
 
 void PCFX_Init(void)
 {
-	if (PCFX_Installed)
-		return;
 
-	PCFX_Stop();
-	TS_ScheduleTask(&PCFX_Service, SND_TICRATE, PCFX_PRIORITY);
-
-	PCFX_Installed = true;
 }
 
 
 void PCFX_Shutdown(void)
 {
-	if (PCFX_Installed)
-	{
-		PCFX_Stop();
-		TS_Terminate(PCFX_PRIORITY);
-		PCFX_Installed = false;
-	}
+
 }
 
 
