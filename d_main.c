b86ec7d489a72cf22516555fe9c955cd051b6e03
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2025-08-21 09:06:38 +0200

    old gcc doesn't have _Noreturn

--- d_main.c
+++ d_main.c
@@ -258,7 +258,8 @@ static void TryRunTics (void)
 //  calls all ?_Responder, ?_Ticker, and ?_Drawer,
 //  calls I_GetTime and I_StartTic
 //
-_Noreturn static void D_DoomLoop(void)
+static void NORETURN_PRE D_DoomLoop(void) NORETURN_POST;
+static void D_DoomLoop(void)
 {
     for (;;)
     {
